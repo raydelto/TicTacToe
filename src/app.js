c0325b8fd07ec34a646505bd4041f5c019c038e8
adalbertofc <cleto321>
2016-03-08 14:36:24 -0400

    almost done

--- src/app.js
+++ src/app.js
@@ -1,79 +1,103 @@
 
 var HelloWorldLayer = cc.Layer.extend({
     sprite:null,
-    ceros: [],
-    cero:null,
-    equis:null,
+    Os:null,
+    Xs:null,
     size:null,
+    tabla:[],
     turno:0,
     random: function getRandomInt(min, max) {
     	return Math.floor(Math.random() * (max - min + 1)) + min;
 	},
 
+     verificar: function(event)
+    {
+        var juego = event.getCurrentTarget();
+        var arr = juego.tabla;
+        console.log(arr);
+        if(arr[0]==arr[1] && arr[1]==arr[2])
+            return arr[0];
+        else if(arr[3]==arr[4] && arr[4]==arr[5])
+            return arr[3];
+        else if(arr[6]==arr[7] && arr[7]==arr[8])
+            return arr[6];
+        else if(arr[0]==arr[3] && arr[3]==arr[6])
+            return arr[0];
+        else if(arr[1]==arr[4] && arr[4]==arr[7])
+            return arr[1];
+        else if(arr[2]==arr[5] && arr[5]==arr[8])
+            return arr[2];
+        else if(arr[0]==arr[4] && arr[4]==arr[8])
+            return arr[0];
+        else if(arr[6]==arr[4] && arr[4]==arr[2])
+            return arr[2];
+        return 0;
+    },
+    
     RealizarJugada : function(location, event)
     {
         var juego = event.getCurrentTarget();
-		var letoca = "";
+		var enturno = "";
         if(juego.turno % 2 == 0)
             {
-                letoca = "O";
+                enturno = "O";
             }
-        else letoca = "X";
+        else enturno = "X";
 			var ubicacion = location.getLocation();
             var x = ubicacion.x;
             var y = ubicacion.y;
         if(x >= 342 && x < 433 && y >= 323 && y < 426 && juego.ceros[0]==null)
             {
-                juego.asignarPos(385,376,letoca);
-                juego.ceros[0]=letoca;
+                juego.asignarPos(385,376,enturno);
+                juego.ceros[0]=enturno;
                 juego.turno++;
             }
-        else if(x >= 433 && x < 526 && y >= 323 && y < 426 && juego.ceros[1]==null)
+        else if(x >= 433 && x < 526 && y >= 323 && y < 426 && juego.tabla[1]==null)
             {
-                juego.asignarPos(481,376,letoca);
-                juego.ceros[1]=letoca;
+                juego.asignarPos(481,376,enturno);
+                juego.tabla[1]=enturno;
                 juego.turno++;
             }
-        else if(x >= 526 && x < 615 && y >= 323 && y < 426 && juego.ceros[2]==null)
+        else if(x >= 526 && x < 615 && y >= 323 && y < 426 && juego.tabla[2]==null)
             {
-                juego.asignarPos(580,376,letoca);
-                juego.ceros[2]=letoca;
+                juego.asignarPos(580,376,enturno);
+                juego.tabla[2]=enturno;
                 juego.turno++;
             }
-        else if(x >= 342 && x < 433 && y >= 221 && y < 323 && juego.ceros[3]==null)
+        else if(x >= 342 && x < 433 && y >= 221 && y < 323 && juego.tabla[3]==null)
             {
-                juego.asignarPos(385,266,letoca);
-                juego.ceros[3]=letoca;
+                juego.asignarPos(385,266,enturno);
+                juego.tabla[3]=enturno;
                 juego.turno++;
             }
-        else if(x >= 433 && x < 526 && y >= 221 && y < 323 && juego.ceros[4]==null)
+        else if(x >= 433 && x < 526 && y >= 221 && y < 323 && juego.tabla[4]==null)
             {
-                juego.asignarPos(481,266,letoca);
-                juego.ceros[4]=letoca;
+                juego.asignarPos(481,266,enturno);
+                juego.tabla[4]=enturno;
                 juego.turno++;
             }
-        else if(x >= 526 && x < 615 && y >= 221 && y < 323 && juego.ceros[5]==null)
+        else if(x >= 526 && x < 615 && y >= 221 && y < 323 && juego.tabla[5]==null)
             {
-                juego.asignarPos(580,266,letoca);
-                juego.ceros[5]=letoca;
+                juego.asignarPos(580,266,enturno);
+                juego.tabla[5]=enturno;
                 juego.turno++;
             }
-        else if(x >= 342 && x < 433 && y >= 116 && y < 221 && juego.ceros[6]==null)
+        else if(x >= 342 && x < 433 && y >= 116 && y < 221 && juego.tabla[6]==null)
             {
-                juego.asignarPos(385,168,letoca);
-                juego.ceros[6]=letoca;
+                juego.asignarPos(385,168,enturno);
+                juego.tabla[6]=enturno;
                 juego.turno++;
             }
-        else if(x >= 433 && x < 526 && y >= 116 && y < 221 && juego.ceros[7]==null)
+        else if(x >= 433 && x < 526 && y >= 116 && y < 221 && juego.tabla[7]==null)
             {
-                juego.asignarPos(481,168,letoca);
-                juego.ceros[7]=letoca;
+                juego.asignarPos(481,168,enturno);
+                juego.tabla[7]=enturno;
                 juego.turno++;
             }
-        else if(x >= 526 && x < 615 && y >= 116 && y < 221 && juego.ceros[8]==null)
+        else if(x >= 526 && x < 615 && y >= 116 && y < 221 && juego.tabla[8]==null)
             {
-                juego.asignarPos(580,168,letoca);
-                juego.ceros[8]=letoca;
+                juego.asignarPos(580,168,enturno);
+                juego.tabla[8]=enturno;
                 juego.turno++;
             }
        
@@ -89,9 +113,9 @@ var HelloWorldLayer = cc.Layer.extend({
         
     },
     
-    asignarPos : function(x,y,t){
-        if(t=="X") pieza = new cc.Sprite(res.X_png);
-        else if(t=="O") pieza = new cc.Sprite(res.O_png);
+    asignarPos : function(x,y,turn){
+        if(turn=="X") pieza = new cc.Sprite(res.X_png);
+        else if(turn=="O") pieza = new cc.Sprite(res.O_png);
         pieza.setPosition(x,y );
         this.addChild(pieza, 1);
         console.log("Se creo un elemento del tipo " + t  +" en " + Math.floor(x) + "," + Math.floor(y));
