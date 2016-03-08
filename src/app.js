db1cd37f772d4e1d96f1c422547f43c1e001cdf7
adalbertofc <cleto321>
2016-03-08 14:26:23 -0400

    functions done

--- src/app.js
+++ src/app.js
@@ -1,8 +1,102 @@
 
 var HelloWorldLayer = cc.Layer.extend({
     sprite:null,
-    
+    ceros: [],
+    cero:null,
+    equis:null,
+    size:null,
+    turno:0,
+    random: function getRandomInt(min, max) {
+    	return Math.floor(Math.random() * (max - min + 1)) + min;
+	},
 
+    RealizarJugada : function(location, event)
+    {
+        var juego = event.getCurrentTarget();
+		var letoca = "";
+        if(juego.turno % 2 == 0)
+            {
+                letoca = "O";
+            }
+        else letoca = "X";
+			var ubicacion = location.getLocation();
+            var x = ubicacion.x;
+            var y = ubicacion.y;
+        if(x >= 342 && x < 433 && y >= 323 && y < 426 && juego.ceros[0]==null)
+            {
+                juego.asignarPos(385,376,letoca);
+                juego.ceros[0]=letoca;
+                juego.turno++;
+            }
+        else if(x >= 433 && x < 526 && y >= 323 && y < 426 && juego.ceros[1]==null)
+            {
+                juego.asignarPos(481,376,letoca);
+                juego.ceros[1]=letoca;
+                juego.turno++;
+            }
+        else if(x >= 526 && x < 615 && y >= 323 && y < 426 && juego.ceros[2]==null)
+            {
+                juego.asignarPos(580,376,letoca);
+                juego.ceros[2]=letoca;
+                juego.turno++;
+            }
+        else if(x >= 342 && x < 433 && y >= 221 && y < 323 && juego.ceros[3]==null)
+            {
+                juego.asignarPos(385,266,letoca);
+                juego.ceros[3]=letoca;
+                juego.turno++;
+            }
+        else if(x >= 433 && x < 526 && y >= 221 && y < 323 && juego.ceros[4]==null)
+            {
+                juego.asignarPos(481,266,letoca);
+                juego.ceros[4]=letoca;
+                juego.turno++;
+            }
+        else if(x >= 526 && x < 615 && y >= 221 && y < 323 && juego.ceros[5]==null)
+            {
+                juego.asignarPos(580,266,letoca);
+                juego.ceros[5]=letoca;
+                juego.turno++;
+            }
+        else if(x >= 342 && x < 433 && y >= 116 && y < 221 && juego.ceros[6]==null)
+            {
+                juego.asignarPos(385,168,letoca);
+                juego.ceros[6]=letoca;
+                juego.turno++;
+            }
+        else if(x >= 433 && x < 526 && y >= 116 && y < 221 && juego.ceros[7]==null)
+            {
+                juego.asignarPos(481,168,letoca);
+                juego.ceros[7]=letoca;
+                juego.turno++;
+            }
+        else if(x >= 526 && x < 615 && y >= 116 && y < 221 && juego.ceros[8]==null)
+            {
+                juego.asignarPos(580,168,letoca);
+                juego.ceros[8]=letoca;
+                juego.turno++;
+            }
+       
+        var ganador = juego.verificar(event);
+        if(ganador==="X") {
+            alert("Ha ganado el jugador X");
+            
+            }
+        if(ganador==="O") {
+            alert("Ha ganado el jugador O");
+            
+            }
+        
+    },
+    
+    asignarPos : function(x,y,t){
+        if(t=="X") pieza = new cc.Sprite(res.X_png);
+        else if(t=="O") pieza = new cc.Sprite(res.O_png);
+        pieza.setPosition(x,y );
+        this.addChild(pieza, 1);
+        console.log("Se creo un elemento del tipo " + t  +" en " + Math.floor(x) + "," + Math.floor(y));
+},
+    
     ctor:function () {
         //////////////////////////////
         // 1. super init first
@@ -37,8 +131,7 @@ var HelloWorldLayer = cc.Layer.extend({
         
         cc.eventManager.addListener({
 			event: cc.EventListener.TOUCH_ONE_BY_ONE,
-			onTouchBegan: function(){console.log("blah");
-                                    }
+			onTouchBegan: RealizarJugada,
 			
 		}, this);
         
